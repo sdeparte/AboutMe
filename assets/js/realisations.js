1600ee7953a5893ac975dd49936f83f19e487af4
Sylvain DEPARTE <sylvain.departe@gmail.com>
2020-09-25 21:10:02 +0200

    Affichage de la description des réalisations

--- assets/js/realisations.js
+++ assets/js/realisations.js
@@ -5,6 +5,8 @@ import '../css/realisations.css';
 
 import $ from 'jquery';
 
+var itemsPerRow;
+
 $( document ).ready(function() {
     $("a[href*=\\#]:not([href=\\#])").click(function() {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -21,4 +23,63 @@ $( document ).ready(function() {
             }
         }
     });
+
+    var containerWidth = $('.list-realisations').outerWidth();
+    var itemWidth = $('.list-realisations > .one-realisation').outerWidth();
+    console.log(itemsPerRow);
+
+    itemsPerRow = Math.floor(containerWidth / itemWidth);
+
+    $('.list-realisations > .one-realisation').on('click', function(){
+        var $triggered = $(this);
+
+        if (!$triggered.hasClass('active')) {
+            $('.list-realisations > .one-realisation').removeClass('active');
+            $('.list-realisations > .one-realisation-description').slideUp();
+
+            var target = $triggered.attr('data-target');
+            var $triggeredElement = $('.list-realisations > .one-realisation-description[data-target='+ target +']');
+
+            positionExpandableElement($triggered, $triggeredElement);
+
+            $triggered.addClass('active');
+            $triggeredElement.slideDown();
+        } else {
+            $('.list-realisations > .one-realisation').removeClass('active');
+            $('.list-realisations > .one-realisation-description').slideUp();
+        }
+    });
 });
+
+$(window).resize(function(){
+    var containerWidth = $('.list-realisations').outerWidth();
+    var itemWidth = $('.list-realisations > .one-realisation').outerWidth();
+
+    var newItemsPerRow = Math.round(containerWidth / itemWidth);
+
+    if (itemsPerRow != newItemsPerRow) {
+        itemsPerRow = newItemsPerRow;
+        console.log(itemsPerRow);
+
+        var $triggered = $('.list-realisations > .one-realisation.active');
+        var target = $triggered.attr('data-target');
+        var $triggeredElement = $('.list-realisations > .one-realisation-description[data-target='+ target +']');
+
+        positionExpandableElement($triggered, $triggeredElement);
+    }
+});
+
+function positionExpandableElement(triggered, element) {
+    var allFlexItems = $('.list-realisations > .one-realisation');
+    var itemsData = [];
+
+    $.each(allFlexItems, function(key, el){
+        itemsData.push($(el).attr('data-target'));
+    });
+
+    var elNumber = $.inArray($(triggered).attr('data-target'), itemsData);
+    var rowNumber = Math.floor(elNumber / itemsPerRow);
+    var insertAfter = (itemsPerRow * rowNumber) + itemsPerRow - 1; //we add itemsPerRow because we always need to skip the first row, -1 because array starts at 0
+
+    $(element).insertAfter($('.list-realisations > .one-realisation')[insertAfter]);
+}
